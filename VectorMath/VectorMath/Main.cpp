07add9caf20aad126f630e00196893fee7bee226
Hojoon Kim <elddy0948@gmail.com>
2022-10-05 22:41:21 +0900

    refactor rename variable name

--- VectorMath/VectorMath/Main.cpp
+++ VectorMath/VectorMath/Main.cpp
@@ -6,8 +6,8 @@ int main(void)
 	Vec3 v(1, 2, 3);
 
 	Vec3 sumUV = u + v;
-	Vec3 abstractUV = u - v;
+	Vec3 subtractionUV = u - v;
 
 	sumUV.ShowVec3();
-	abstractUV.ShowVec3();
+	subtractionUV.ShowVec3();
 }
